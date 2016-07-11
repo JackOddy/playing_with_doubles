4dc386dee2d9441e170ca5b22f83b8a8e0b5085d
Jack Oddy <jackoddy91@gmail.com>
2016-07-11 21:15:09 +0100

    fixes errors and gives a rewarding test title

--- spec/foo_spec.rb
+++ spec/foo_spec.rb
@@ -3,8 +3,9 @@ require 'foo'
 
 
 describe Foo do
+  let(:object) { double(:object, msg: 'I did it!') }
   describe '#bar' do
-    it 'returns the string "foobar"' do
+    it 'you did it, well done!' do
       expect(subject.bar(object)).to eq 'I did it!' # do not change this line!
     end
   end
