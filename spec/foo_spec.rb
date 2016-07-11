a7178c3dbbc3dd432f0a61afc66d30df5eed74cb
Jack Oddy <jackoddy91@gmail.com>
2016-07-11 21:16:02 +0100

    corrects typo...

--- spec/foo_spec.rb
+++ spec/foo_spec.rb
@@ -3,7 +3,6 @@ require 'foo'
 
 
 describe Foo do
-  let(:object) { double(:object, msg: 'I did it!') }
   describe '#bar' do
     it 'you did it, well done!' do
       expect(subject.bar(object)).to eq 'I did it!' # do not change this line!
